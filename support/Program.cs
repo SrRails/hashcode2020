f52c84ee426676afb3a569b8bb841c6aa4a01b8a
fernaa51 <alvaro.fernandez.af4@roche.com>
2020-02-13 17:31:17 +0100

    Add sample code and file

--- support/Program.cs
+++ support/Program.cs
@@ -1,13 +1,30 @@
 using System;
+using System.IO;
+using System.Text;
+
 namespace hascode2020
 {
-  internal static class Program
-  {
-    private static void Main(string[] args)
+    internal static class Program
     {
-      Console.WriteLine("pamelachuunkitopo...");
-      Environment.Exit(0);
-    }
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("pamelachuunkitopo...");
+            var filePath="fileForTest.txt";
+            Console.WriteLine(ReadFile(filePath));
+            Environment.Exit(0);
+        }
 
-  }
+        private static string ReadFile(string filePath)
+        {
+            StringBuilder stringBuilder=new StringBuilder();
+            FileStream fileStream = new FileStream(filePath, FileMode.Open);
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+                string content=reader.ReadToEnd();
+                stringBuilder.Append(content);
+            }
+            return stringBuilder.ToString();
+        }
+
+    }
 }
