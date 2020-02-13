bbf570b2c7b4835d4a3ea3a5e554a409d5b0dd25
fernaa51 <alvaro.fernandez.af4@roche.com>
2020-02-13 15:37:12 +0100

    add main method with something for Raul

--- support/Program.cs
+++ support/Program.cs
@@ -3,6 +3,11 @@ namespace hascode2020
 {
   internal static class Program
   {
-      
+    private static void Main(string[] args)
+    {
+      Console.WriteLine("pamelachuunkitopo...");
+      Environment.Exit(0);
+    }
+
   }
 }
