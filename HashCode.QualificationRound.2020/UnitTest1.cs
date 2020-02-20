54c6cd6193615466e2e9d43ca6b2b66107eaf80b
Corrado Calzoni <corrado.calzoni@roche.com>
2020-02-20 19:11:23 +0100

    Create Input

--- HashCode.QualificationRound.2020/UnitTest1.cs
+++ HashCode.QualificationRound.2020/UnitTest1.cs
@@ -2,9 +2,29 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace HashCode.QualificationRound._2020
 {
+    public class Library
+    {
+        public int DaysToSign { get; set; }
+
+        public int BookPerDay { get; set; }
+
+        public int[] Books { get; set; }
+    }
+
+    public class Input
+    {
+        public Library[] Libraries { get; set; }
+
+        public int[] Catalog { get; set; }
+
+        public int DaysForScanning { get; set; }
+    }
+
+
     [TestClass]
     public class UnitTest1
     {
+        
         [TestMethod]
         public void TestMethod1()
         {
