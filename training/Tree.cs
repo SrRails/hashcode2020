571d38fb00661c534a32ae1611f2f531d8a834af
Fernandez <fernaa51@rtcuv9009.emea.roche.com>
2020-02-20 17:58:17 +0100

    Add tree

---
+++ training/Tree.cs
@@ -0,0 +1,25 @@
+public class Tree
+{
+    Node top;
+
+    public Node Top { get => top; set => top = value; }
+
+    public void Add();
+    public void AddRecursive();
+    public void Print();
+}
+
+public class Node
+{
+    public int value;
+
+    public Node left;
+    public Node right;
+
+    public Tree(Parameters)
+    {
+        
+    }
+
+
+}
